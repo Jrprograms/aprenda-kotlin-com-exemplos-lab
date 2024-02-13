e0deee91dd727972b6f5e9b4cd39eeb436a8c23e
Ivan Jr <86005589+Jrprograms@users.noreply.github.com>
2024-02-13 17:57:49 -0300

    Modificações solicitadas do projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,64 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(var nome: String, val email: String, val senha: String){
+    override fun toString(): String{
+		return this.nome
+    }
+}
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracaoMinutos: Float = 60f, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional> ) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    override fun toString(): String{
+        return (this.nome)
+    }
+    
+    fun matricular(usuario: Usuario): Unit {
+        this.inscritos.add(usuario)
+        println("Usuario $usuario adicionado!")
+        println(this.inscritos)
+    }
+    
+    fun qtdMatriculas(): Unit{
+		println(this.inscritos.size)
+    }
+    
+    fun removerMatricula(usuario: Usuario): Unit{
+		if(this.inscritos.remove(usuario)){
+			println("Usuario $usuario removido da formação $this com sucesso!")
+        }else{
+			println("Usuário $usuario não encontrado")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //Inicialização de variáveis
+    val usuario = Usuario("Joao","Batata@gmail.com","1234")
+    val usuario2 = Usuario("Alisson", "DIO@gmail.com","1234")
+    val usuario3 = Usuario("Joao","email@gmail.com","5421")
+    
+    val aprendendoLogica = ConteudoEducacional("Aprendendo Logica",80f, Nivel.BASICO)
+    val aprendendoJs = ConteudoEducacional("Aprendendo JavaScript",120f, Nivel.INTERMEDIARIO)
+    val conteudo: List<ConteudoEducacional> = listOf(aprendendoLogica, aprendendoJs)
+    val react = Formacao("React para iniciantes", conteudo)
+    
+    //Teste de métodos
+    
+    //Matriculando e removendo usuario
+    react.matricular(usuario)
+    react.qtdMatriculas()
+    
+    react.removerMatricula(usuario)
+    react.qtdMatriculas()
+    
+    //Tentativa de matricular um usuario com um mesmo nome
+    react.matricular(usuario)
+    
+    //Remover um usuário não matriculado
+    react.removerMatricula(usuario2)
+    
 }
